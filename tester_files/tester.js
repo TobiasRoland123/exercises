90cc85928745cd33a3be4ab7cfd90c7b4940e042
Samuel Tobias Roland Uyet <samu1493@stud.kea.dk>
2023-02-20 07:18:20 +0100

    ..

--- tester_files/tester.js
+++ tester_files/tester.js
@@ -71,47 +71,114 @@
 
 // console.log(sayHi("Tobias"));
 
-const person3 = {
-  firstName: "Harry",
-  lastName: "Potter",
-  hired: false,
-};
+// const person3 = {
+//   firstName: "Harry",
+//   lastName: "Potter",
+//   hired: false,
+// };
 
-const person4 = {
-  firstName: "Fred",
-  lastName: "Weasley",
-  hired: false,
-};
+// const person4 = {
+//   firstName: "Fred",
+//   lastName: "Weasley",
+//   hired: false,
+// };
 
-function hire(person) {
-  person.hired = true;
-}
+// function hire(person) {
+//   person.hired = true;
+// }
 
-function fire(person) {
-  person.hired = false;
-}
+// function fire(person) {
+//   person.hired = false;
+// }
 
-function fireOrHire(action, person) {
-  action(person);
-}
+// function fireOrHire(action, person) {
+//   action(person);
+// }
 
-fireOrHire(hire, person4);
+// fireOrHire(hire, person4);
 
-fireOrHire(fire, person3);
+// fireOrHire(fire, person3);
 
 // console.log(person3);
 // console.log(person4);
 
-console.log(`This is person3: 
-${person3.firstName}
-${person3.lastName}
-${person3.hired}
+// console.log(`This is person3:
+// ${person3.firstName}
+// ${person3.lastName}
+// ${person3.hired}
 
-`);
+// `);
 
-console.log(`This is person4: 
-${person4.firstName}
-${person4.lastName}
-${person4.hired}
+// console.log(`This is person4:
+// ${person4.firstName}
+// ${person4.lastName}
+// ${person4.hired}
 
-`);
+// `);
+
+// loadJSON("students.json", showJsonData);
+
+// async function loadJSON(url, callback) {
+//   const response = await fetch(url);
+//   const jsonData = await response.json();
+
+//   callback(jsonData);
+// }
+
+// function showJsonData(jsonData) {
+//   console.table(jsonData);
+// }
+
+// loadJSON("students.json", showJsonData);
+
+// async function loadJSON(url, callback) {
+//   const response = await fetch(url);
+//   const jsonData = await response.json();
+
+//   callback(jsonData);
+// }
+
+// function showJsonData(jsonData) {
+//   jsonData.forEach(showName);
+// }
+
+// function showName(student) {
+//   console.log(student.fullname);
+// }
+
+const animals = [
+  { name: "mandu", type: "cat" },
+  { name: "Mia", type: "cat" },
+  { name: "bob", type: "dog" },
+  { name: "scooby", type: "dog" },
+];
+
+function all() {
+  return true;
+}
+
+function none() {
+  return false;
+}
+
+console.log("ALL ", animals.filter(all));
+console.log("NONE ", animals.filter(none));
+
+function isCat(animal) {
+  if (animal.type === "cat") {
+    return true;
+  } else {
+    return false;
+  }
+}
+function isDog(animal) {
+  if (animal.type === "dog") {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log("Cats only ", animals.filter(isCat));
+
+console.log("Dogs only ", animals.filter(isDog));
