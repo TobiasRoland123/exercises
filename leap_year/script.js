ae913722c0a5b989f64b56de80b2394e05c9f1be
Samuel Tobias Roland Uyet <samu1493@stud.kea.dk>
2023-02-21 14:44:26 +0100

    leapyear checker works

--- leap_year/script.js
+++ leap_year/script.js
@@ -1,1 +1,17 @@
 "use strict";
+
+let inputYear = 2000;
+
+console.log(checkYear(inputYear));
+
+function checkYear(year) {
+  let isLeapYear = false;
+
+  if (year % 4 === 0 && year % 100 != 0) {
+    isLeapYear = true;
+  } else if (year % 4 === 0 && year % 100 === 0 && year % 400 === 0) {
+    isLeapYear = true;
+  }
+
+  return isLeapYear;
+}
